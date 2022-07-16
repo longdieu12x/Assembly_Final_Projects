b0689cfa37a47c98a60ed0654f999284ea16b85e
dang.pm.IT <74671798+longdieu12x@users.noreply.github.com>
2022-07-17 01:15:19 +0700

    upload file prj10.asm

--- prj10.asm
+++ prj10.asm
@@ -15,8 +15,8 @@
 .eqv RIGHT_LED            0xFFFF0010
 .data
 header: .asciiz "Welcome to Minh Dang and Minh Khoi Calculator \n"
-inputNumberOne: .asciiz "Please input first number Ex: 0123 \n"
-inputNumberTwo: .asciiz "Please input second number Ex: 0234 \n"
+inputNumberOne: .asciiz "Please input first number Ex: 23 \n"
+inputNumberTwo: .asciiz "Please input second number Ex: 34 \n"
 inputOperator: .asciiz "Press a to add two number \nPress b to subtract two number\nPress c to multiply two number\nPress d to divide two number \n"
 alreadyInputOne: .asciiz "You already input first number: "
 alreadyInputTwo: .asciiz "You already input second number: "
@@ -31,7 +31,9 @@ thanks: .asciiz "Thanks for using this calculator \n"
 error: .asciiz "Error happened please try again! \n"
 arrHexLeds: .word 63, 6, 91, 79, 102, 109, 125, 7, 127, 111
 nextline: .asciiz "\n"
-
+runhere: .asciiz "Run here"
+runhere1: .asciiz "RUnhere 1"
+runhere2: .asciiz "RUn here 2"
 .text
 welcome:
   li $v0, 4
@@ -66,18 +68,11 @@ li $s5, 4 # length of number is 4
 LoopNumberOne:
   beq $t0, $s5, GetFirstNumber
   nop
-  beq $t0, $s5, GetFirstNumber
-  nop
-  beq $t0, $s5, GetFirstNumber
-  nop
-  beq $t0, $s5, GetFirstNumber
-  nop
   b LoopNumberOne
   beq $t0, $s5, GetFirstNumber
   nop
-  beq $t0, $s5, GetFirstNumber
-  nop
   b LoopNumberOne
+
 
 GetFirstNumber:
   add $s3, $s1, $0
@@ -106,12 +101,6 @@ AlertNumberTwo:
 LoopNumberTwo:
   beq $t0, $s5, GetSecondNumber
   nop
-  beq $t0, $s5, GetSecondNumber
-  nop
-  beq $t0, $s5, GetSecondNumber
-  nop
-  beq $t0, $s5, GetSecondNumber
-  nop
   b LoopNumberTwo
   beq $t0, $s5, GetSecondNumber
   nop
@@ -122,7 +111,7 @@ GetSecondNumber:
   addi $s1, $0, 0 # reset input number
   addi $t0, $0, 0  # reset count number
 
-  # print first number to console
+  # print second number to console
   li $v0, 4
   la $a0, alreadyInputTwo
   syscall
@@ -180,7 +169,7 @@ AlertEqual:
   syscall
 
 LoopEqual:
-  beq $s7, 6 ,getEqual
+  beq $s7, 1 ,getEqual
   b LoopEqual
 
 getEqual:
@@ -336,41 +325,41 @@ handleInInterruptRowOne:
   addi $sp,$sp,4
   sw $ra,0($sp) 
   li $t1,IN_ADDRESS_HEXA_KEYBOARD
-  li $t3,0x81     # Kich hoat interrupt, cho phep bam phim o hang 1
+  li $t3,0x81     # Allow choose row 1
   sb $t3,0($t1)
   li $t1,OUT_ADDRESS_HEXA_KEYBOARD
-  lb $t3,0($t1)   # Nhan byte the hien vi tri cua phim duoc bam trong hang 1
+  lb $t3,0($t1)   # Load value of row 1
   case_interrupt_0:
     li $t5,0x11
     bne $t3, $t5 ,case_interrupt_1  # case 0x11
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,0  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,0  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptOne
   case_interrupt_1:
     li $t5,0x21
     bne $t3, $t5 ,case_interrupt_2  # case 0x12
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,1  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,1  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptOne
   case_interrupt_2:
     li $t5,0x41
     bne $t3, $t5 ,case_interrupt_3  # case 0x13
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,2  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,2  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptOne
   case_interrupt_3:
     li $t5, 0xffffff81
     bne $t3, $t5 , getInterruptOne  # case 0x14
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,3  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,3  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptOne
   showInInterruptOne:
     li $t2,RIGHT_LED   # set light on LED right
@@ -391,41 +380,41 @@ handleInInterruptRowTwo:
   addi $sp,$sp,4
   sw $ra,0($sp) 
   li $t1,IN_ADDRESS_HEXA_KEYBOARD
-  li $t3,0x82     # Kich hoat interrupt, cho phep bam phim o hang 2
+  li $t3,0x82     # Allow choose row 2
   sb $t3,0($t1)
   li $t1,OUT_ADDRESS_HEXA_KEYBOARD
-  lb $t3,0($t1)   # Nhan byte the hien vi tri cua phim duoc bam trong hang 2
+  lb $t3,0($t1)   # Load value of row 2
   case_interrupt_4:
     li $t5,0x12
     bne $t3, $t5 ,case_interrupt_5  # case 0x11
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,4  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,4  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptTwo
   case_interrupt_5:
     li $t5,0x22
     bne $t3, $t5 ,case_interrupt_6  # case 0x12
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,5  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,5  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptTwo
   case_interrupt_6:
     li $t5,0x42
     bne $t3, $t5 ,case_interrupt_7  # case 0x13
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,6  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,6  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptTwo
   case_interrupt_7:
     li $t5, 0xffffff82
     bne $t3, $t5 , getInterruptTwo  # case 0x14
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,7  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,7  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptTwo
   showInInterruptTwo:
     li $t2,RIGHT_LED   # set light on LED right
@@ -447,25 +436,25 @@ handleInInterruptRowThree:
   addi $sp,$sp,4
   sw $ra,0($sp) 
   li $t1,IN_ADDRESS_HEXA_KEYBOARD
-  li $t3,0x84     # Kich hoat interrupt, cho phep bam phim o hang 2
+  li $t3,0x84     # Allow choose row 2
   sb $t3,0($t1)
   li $t1,OUT_ADDRESS_HEXA_KEYBOARD
-  lb $t3,0($t1)   # Nhan byte the hien vi tri cua phim duoc bam trong hang 2
+  lb $t3,0($t1)   # Load value of row 2
   case_interrupt_8:
     li $t5,0x00000014
     bne $t3, $t5 ,case_interrupt_9  # case 0x11
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,8  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,8  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptThree
   case_interrupt_9:
     li $t5,0x00000024
     bne $t3, $t5 ,case_interrupt_a  # case 0x12
-    addi $a3,$a2,0    # left=right
-    addi $a2,$zero,9  # left = 0
+    addi $a3,$a2,0    # store left value
+    addi $a2,$zero,9  # set right value
     mul $s1,$s1,10
-    add $s1,$s1,$a2   # factor=factor*10+left
+    add $s1,$s1,$a2   # implement s1 = s1 * 10 + right value to get value of full number
     j showInInterruptThree
   case_interrupt_a:
     li $t5,0x44
@@ -496,10 +485,10 @@ handleInInterruptRowFour:
   addi $sp,$sp,4
   sw $ra,0($sp) 
   li $t1,IN_ADDRESS_HEXA_KEYBOARD
-  li $t3,0x88     # Kich hoat interrupt, cho phep bam phim o hang 2
+  li $t3,0x88     # Allow choose row 2
   sb $t3,0($t1)
   li $t1,OUT_ADDRESS_HEXA_KEYBOARD
-  lb $t3,0($t1)   # Nhan byte the hien vi tri cua phim duoc bam trong hang 2
+  lb $t3,0($t1)   # Load value of row 2
   case_interrupt_c:
     li $t5,0x18
     bne $t3, $t5 ,case_interrupt_d  # case 0x13
@@ -513,7 +502,7 @@ handleInInterruptRowFour:
   case_interrupt_f:
     li $t5, 0xffffff88 
     bne $t3, $t5 , getInterruptFour # case 0x14
-    addi $s7, $0, 6  
+    addi $s7, $0, 1  
     j getInterruptFour
   getInterruptFour:
     lw $ra, 0($sp)
